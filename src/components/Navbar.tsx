67454f648e117f57abb376dd2df1e6bcab8011b6
ekaputra04 <ekaputrajuniawan@gmail.com>
2025-02-17 22:59:25 +0800

    chore: init project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,59 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+
+const NavMenuItems = [
+  {
+    name: "Home",
+    href: "#",
+  },
+  {
+    name: "About Us",
+    href: "#",
+  },
+  {
+    name: "Projects",
+    href: "#",
+  },
+  {
+    name: "Services",
+    href: "#",
+  },
+  {
+    name: "Contact Us",
+    href: "#",
+  },
+];
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="flex justify-between lg:px-32 py-4">
+        <div className="flex items-center gap-4">
+          <img src="../../public/images/logo.png" alt="" className="h-8" />
+          <img src="../../public/images/TheBox.png" alt="" className="h-4" />
+        </div>
+        <div className="">
+          <NavigationMenu>
+            <NavigationMenuList>
+              {NavMenuItems.map((item, index) => (
+                <NavigationMenuItem key={index}>
+                  <NavigationMenuLink
+                    className={navigationMenuTriggerStyle()}
+                    href={item.href}
+                  >
+                    {item.name}
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              ))}
+            </NavigationMenuList>
+          </NavigationMenu>
+        </div>
+      </div>
+    </>
+  );
+}
